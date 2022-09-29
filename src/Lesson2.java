2f0d3b9d736baf9a0832e943491e10620f28ad2d
Desertt <mustafadagtekin@gmail.com>
2022-09-30 01:10:19 +0300

    Welcome To JavaBootcamp Added Lesson2

---
+++ src/Lesson2.java
@@ -0,0 +1,35 @@
+import java.sql.SQLOutput;
+
+public class Lesson2 {
+
+    public static void main(String[] args) {
+
+        //byte kullanımı // Byte Using
+        byte myBit = 120;
+        System.out.println(myBit);
+
+        //Short Kullanımı // Short Using
+        short myShort = 130;
+        System.out.println(myShort);
+
+        //CTRL + ALT + L  --> Kodları düzenlemeniz için kullanılır. // CTRL + ALT + L  --> Used to edit/format codes.
+
+        //integer kullanımı // İnteger Using
+        int myInt = 140;
+        System.out.println(myInt);
+
+        //long kullanımı // long Using
+        long myLong = 151 ;
+        System.out.println(myLong);
+
+        //Float Kullanımı //Float Using
+        float myFloat = 1.2f;    // Float'ı tanıması için Float'ın f sini yazmalısınız.
+        System.out.println(myFloat);
+
+        //Double Kullanımı
+        double myDouble = 1.4;
+        System.out.println(myDouble);
+
+    }
+
+}
